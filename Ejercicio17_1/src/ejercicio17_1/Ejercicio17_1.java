8c974f44191ac3b4eb524f4f295162ccfc16612f
artur <artur@ArturoMSI>
2018-01-22 09:19:13 +0100

    Ejercicio terminado

---
+++ Ejercicio17_1/src/ejercicio17_1/Ejercicio17_1.java
@@ -0,0 +1,17 @@
+package ejercicio17_1;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio17_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Numeros num=new Numeros();
+        num.arrayNum();
+    }
+
+}
