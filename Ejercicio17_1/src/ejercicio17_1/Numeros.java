7cdf0ed4d4c6bc0be41685173a4d2b2edb3523ce
artur <artur@ArturoMSI>
2018-01-23 13:22:52 +0100

    Añadido metodo para dar la vuelta y mostrar

--- Ejercicio17_1/src/ejercicio17_1/Numeros.java
+++ Ejercicio17_1/src/ejercicio17_1/Numeros.java
@@ -1,20 +1,36 @@
 package ejercicio17_1;
 
 import static java.lang.Math.random;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
  * @author Arturo
  */
-public class Numeros {
-       
+public class Numeros{
+
+    int[] numeros=new int[6];
+
     public void arrayNum(){
-        int[] numeros =new int[6];
-        
-        for(int i=0;i<numeros.length;i++){
-            numeros[i]=ThreadLocalRandom.current().nextInt(1,50+1);
+
+        for(int i=0; i<numeros.length; i++){
+            numeros[i]=ThreadLocalRandom.current().nextInt(1, 50+1);
             System.out.println(numeros[i]);
         }
+    }
+    public void mostrarArray(){
+        for(int i=0; i<numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+    }
+
+    public int[] reverseArray(){
+        for(int i=0; i<numeros.length/2; i++){
+            int temp=numeros[i];
+            numeros[i]=numeros[numeros.length-i-1];
+            numeros[numeros.length-i-1]=temp;
+        }
+        return numeros;
     }
 }
